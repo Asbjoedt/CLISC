c2dcbb13fe6747dbaf6059cf0e95afaa979198ac
Asbjoedt <asbjoern.skoedt@gmail.com>
2022-08-02 23:28:03 +0200

    Commit

--- CLISC/Archive_Dataquality.cs
+++ CLISC/Archive_Dataquality.cs
@@ -166,6 +166,22 @@ namespace CLISC
             }
         }
 
+        public bool Simple_Alert_EmbeddedObjects(string filepath)
+        {
+            using (SpreadsheetDocument spreadsheetDoc = SpreadsheetDocument.Open(filepath, true))
+            {
+                WorksheetPart sourceSheetPart = GetWorksheetPartByName(spreadsheetDoc, "Test");
+
+                var imagePart = sourceSheetPart.AddImagePart(ImagePartType.Emf, "rId1");
+                imagePart.FeedData(File.Open(placeholderImagePath, FileMode.Open));
+
+                var embeddedObject =
+                    sourceSheetPart.AddEmbeddedObjectPart(@"application/vnd.openxmlformats-officedocument.oleObject");
+                embeddedObject.FeedData(File.Open(embeddedFilePath, FileMode.Open));
+
+            }
+            return true;
+        }
 
         // Check for embedded objects and return alert
         public string Alert_EmbeddedObjects(string filepath)
@@ -211,6 +227,99 @@ namespace CLISC
                 }
 
             }
+        }
+
+        // Get all worksheets in a spreadsheet
+        // Source: https://docs.microsoft.com/en-us/office/open-xml/how-to-retrieve-a-list-of-the-worksheets-in-a-spreadsheet
+        public static Sheets GetAllWorksheets(string filepath)
+        {
+            Sheets theSheets = null;
+
+            using (SpreadsheetDocument document = SpreadsheetDocument.Open(filepath, false))
+            {
+                WorkbookPart wbPart = document.WorkbookPart;
+                theSheets = wbPart.Workbook.Sheets;
+            }
+            return theSheets;
+        }
+
+        // Retrieve the value of a cell, given a file name, sheet name, and address name.
+        // Source: https://docs.microsoft.com/en-us/office/open-xml/how-to-retrieve-the-values-of-cells-in-a-spreadsheet
+        public static string GetCellValue(string fileName, string sheetName, string addressName)
+        {
+            string value = null;
+
+            // Open the spreadsheet document for read-only access.
+            using (SpreadsheetDocument document = SpreadsheetDocument.Open(fileName, false))
+            {
+                // Retrieve a reference to the workbook part.
+                WorkbookPart wbPart = document.WorkbookPart;
+
+                // Find the sheet with the supplied name, and then use that 
+                // Sheet object to retrieve a reference to the first worksheet.
+                Sheet theSheet = wbPart.Workbook.Descendants<Sheet>().
+                  Where(s => s.Name == sheetName).FirstOrDefault();
+
+                // Throw an exception if there is no sheet.
+                if (theSheet == null)
+                {
+                    throw new ArgumentException("sheetName");
+                }
+
+                // Retrieve a reference to the worksheet part.
+                WorksheetPart wsPart = (WorksheetPart)(wbPart.GetPartById(theSheet.Id));
+
+                // Use its Worksheet property to get a reference to the cell 
+                // whose address matches the address you supplied.
+                Cell theCell = wsPart.Worksheet.Descendants<Cell>().
+                  Where(c => c.CellReference == addressName).FirstOrDefault();
+
+                // If the cell does not exist, return an empty string.
+                if (theCell.InnerText.Length > 0)
+                {
+                    value = theCell.InnerText;
+
+                    // If the cell represents an integer number, you are done. For dates, this code returns the serialized value that represents the date. The code handles strings and Booleans individually. For shared strings, the code looks up the corresponding value in the shared string table. For Booleans, the code converts the value into the words TRUE or FALSE.
+                    if (theCell.DataType != null)
+                    {
+                        switch (theCell.DataType.Value)
+                        {
+                            case CellValues.SharedString:
+
+                                // For shared strings, look up the value in the
+                                // shared strings table.
+                                var stringTable =
+                                    wbPart.GetPartsOfType<SharedStringTablePart>()
+                                    .FirstOrDefault();
+
+                                // If the shared string table is missing, something 
+                                // is wrong. Return the index that is in
+                                // the cell. Otherwise, look up the correct text in 
+                                // the table.
+                                if (stringTable != null)
+                                {
+                                    value =
+                                        stringTable.SharedStringTable
+                                        .ElementAt(int.Parse(value)).InnerText;
+                                }
+                                break;
+
+                            case CellValues.Boolean:
+                                switch (value)
+                                {
+                                    case "0":
+                                        value = "FALSE";
+                                        break;
+                                    default:
+                                        value = "TRUE";
+                                        break;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            return value;
         }
     }
 }
--- CLISC/Convert_OOXML.cs
+++ CLISC/Convert_OOXML.cs
@@ -6,9 +6,6 @@ using System.Threading.Tasks;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
-using Microsoft.Office.Interop.Excel; // Use with XLSB
-using Excel = Microsoft.Office.Interop.Excel; // Use with XLSB
-using System.IO.Compression; // Use with Transitional to Strict
 
 namespace CLISC
 {
@@ -35,41 +32,17 @@ namespace CLISC
         // Convert XLSX Strict to Transitional conformance - WORK IN PROGRESS
         public bool Convert_Strict_to_Transitional(string input_filepath, string output_filepath, string file_folder)
         {
+            using (var spreadsheet = SpreadsheetDocument.Open(file, false))
+            {
+                // Check for Strict conformance class
+                WorkbookPart wbPart = spreadsheet.WorkbookPart;
+                var conformance = wbPart.Workbook.Conformance;
 
+                // If Strict, transform
+                if (conformance != null || conformance == "transitional")
+                {
 
-            bool convert_success = true;
-            return convert_success;
-        }
-
-        // Convert XLSB using Excel
-        // Found code here: https://docs.microsoft.com/en-us/answers/questions/212363/how-to-convert-xlsb-file-to-xlsx.html
-        // NOT USED IN PROGRAM - it needs Excel installed
-        public bool Convert_XLSB(string org_filepath, string input_filepath, string output_filepath)
-        {
-            // Create object instance
-            Excel.Application excelApplication = new Excel.Application();
-            Workbooks workbooks = excelApplication.Workbooks;
-
-            // open book in any format
-            Excel.Workbook workbook = workbooks.Open(input_filepath, XlUpdateLinks.xlUpdateLinksNever, true, Type.Missing, Type.Missing, Type.Missing,
-                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-
-            // save in XlFileFormat.xlExcel12 format which is XLSB
-            workbook.SaveAs(output_filepath, XlFileFormat.xlOpenXMLWorkbook, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-                XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-
-            // close workbook
-            workbook.Close(false, Type.Missing, Type.Missing);
-            excelApplication.Quit();
-
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(workbooks);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApplication);
-
-            //foreach (system.diagnostics.process proc in system.diagnostics.process.getprocessesbyname("excel"))
-            //{
-            //    proc.kill();
-            //}
+                }
 
             bool convert_success = true;
             return convert_success;
