a7741ba879871fac20fcf2f29fae7f78a3b7c89b
Asbjoedt <asbjoern.skoedt@gmail.com>
2022-09-04 14:33:44 +0200

    Commit

--- CLISC/Convert_OOXML.cs
+++ CLISC/Convert_OOXML.cs
@@ -33,16 +33,26 @@ namespace CLISC
         }
 
         // Convert .xlsx Strict to Transitional conformance
-        public bool Convert_Strict_to_Transitional(string input_filepath)
+        public void Convert_Strict_to_Transitional(string input_filepath)
         {
             string namespace_xmlns = "http://schemas.openxmlformats.org/spreadsheetml/2006/main";
             string namespace_xmlns_r = "http://schemas.openxmlformats.org/officeDocument/2006/relationships";
-            string namespace_app_xlmns = "http://schemas.openxmlformats.org/officeDocument/2006/extended-properties";
+            string namespace_app_xmlns = "http://schemas.openxmlformats.org/officeDocument/2006/extended-properties";
             string namespace_app_xmlns_vt = "http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes";
             string namespace_xmlns_dc = ""; // not relevant
             string namespace_xmlns_dcterms = ""; // not relevant
             string namespace_xmlns_dcmitype = ""; // not relevant
             string namespace_xmlns_a = "http://schemas.openxmlformats.org/drawingml/2006/main";
+            string namespace_xmlns_v = ""; // not relevant
+            string namespace_rel_styles = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles";
+            string namespace_rel_themes = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme";
+            string namespace_rel_worksheet = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet";
+            string namespace_rel_sharedstrings = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings";
+            string namespace_rel_externallink = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/externalLink";
+            string namespace_rel_workbook = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument";
+            string namespace_externallink_externallinkpath = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/externalLinkPath";
+            string namespace_externallink_oleobject = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/oleObject";
+            string namespace_drawing_xmlns_xdr = "http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing";
 
             using (var spreadsheet = SpreadsheetDocument.Open(input_filepath, true))
             {
@@ -72,41 +82,48 @@ namespace CLISC
                         }
                     }
                     // Change namespaces in /xl/styles.xml
-                    wbPart.WorkbookStylesPart.Stylesheet.RemoveNamespaceDeclaration("x");
-                    wbPart.WorkbookStylesPart.Stylesheet.AddNamespaceDeclaration("x", namespace_xmlns);
-
+                    if (wbPart.WorkbookStylesPart.Stylesheet != null)
+                    {
+                        wbPart.WorkbookStylesPart.Stylesheet.RemoveNamespaceDeclaration("x");
+                        wbPart.WorkbookStylesPart.Stylesheet.AddNamespaceDeclaration("x", namespace_xmlns);
+                    }
                     // Change namespaces in /xl/sharedStrings.xml
                     if (wbPart.SharedStringTablePart != null)
                     {
                         wbPart.SharedStringTablePart.SharedStringTable.RemoveNamespaceDeclaration("x");
                         wbPart.SharedStringTablePart.SharedStringTable.AddNamespaceDeclaration("x", namespace_xmlns);
                     }
-
-
-                    // Change namespaces in /xl/embeddings
-
                     // Change namespaces in /xl/externallinks
                     List<ExternalWorkbookPart> extwbParts = wbPart.ExternalWorkbookParts.ToList();
                     if (extwbParts.Count > 0)
                     {
                         foreach (ExternalWorkbookPart extwbPart in extwbParts)
                         {
-
+                            extwbPart.RootElement.RemoveNamespaceDeclaration("x");
+                            extwbPart.RootElement.AddNamespaceDeclaration("x", namespace_xmlns);
+                            extwbPart.RootElement.RemoveNamespaceDeclaration("r");
+                            extwbPart.RootElement.AddNamespaceDeclaration("r", namespace_xmlns_r);
                         }
                     }
-                    // Change namespaces in /docProps/app.xml
-
-
-                    // Change namespaces in /docProps/core.xml
-
-
+                    // Change namespaces in /docProps/app.xml (IS THIS WORKING?)
+                    spreadsheet.ExtendedFilePropertiesPart.Properties.RemoveNamespaceDeclaration("x");
+                    spreadsheet.ExtendedFilePropertiesPart.Properties.AddNamespaceDeclaration("x", namespace_app_xmlns);
+                    spreadsheet.ExtendedFilePropertiesPart.Properties.RemoveNamespaceDeclaration("vt");
+                    spreadsheet.ExtendedFilePropertiesPart.Properties.AddNamespaceDeclaration("vt", namespace_app_xmlns_vt);
                     // Change namespaces in /xl/theme/theme[n+1].xml
-                    wbPart.ThemePart.Theme.RemoveNamespaceDeclaration("a");
-                    wbPart.ThemePart.Theme.AddNamespaceDeclaration("a", namespace_xmlns_a);
+                    if (wbPart.ThemePart.Theme != null)
+                    {
+                        wbPart.ThemePart.Theme.RemoveNamespaceDeclaration("a");
+                        wbPart.ThemePart.Theme.AddNamespaceDeclaration("a", namespace_xmlns_a);
+                    }
+                    // Change namespaces in /xl/calcChain.xml
+                    if (wbPart.CalculationChainPart.CalculationChain != null)
+                    {
+                        wbPart.CalculationChainPart.CalculationChain.RemoveNamespaceDeclaration("x");
+                        wbPart.CalculationChainPart.CalculationChain.AddNamespaceDeclaration("x", namespace_xmlns);
+                    }
                 }
             }
-            bool convert_success = true;
-            return convert_success;
         }
 
         // Convert .xlsx Transtional to Strict
@@ -114,7 +131,7 @@ namespace CLISC
         {
             string namespace_xmlns = "http://purl.oclc.org/ooxml/spreadsheetml/main";
             string namespace_xmlns_r = "http://purl.oclc.org/ooxml/officeDocument/relationships";
-            string namespace_app_xlmns = "http://purl.oclc.org/ooxml/officeDocument/extendedProperties";
+            string namespace_app_xmlns = "http://purl.oclc.org/ooxml/officeDocument/extendedProperties";
             string namespace_app_xmlns_vt = "http://purl.oclc.org/ooxml/officeDocument/docPropsVTypes";
             string namespace_core_xmlns_dc = "http://purl.org/dc/elements/1.1/"; // not relevant
             string namespace_core_xmlns_dcterms = "http://purl.org/dc/terms/"; // not relevant
@@ -128,6 +145,7 @@ namespace CLISC
             string namespace_rel_externallink = "http://purl.oclc.org/ooxml/officeDocument/relationships/externalLink";
             string namespace_rel_workbook = "http://purl.oclc.org/ooxml/officeDocument/relationships/officeDocument";
             string namespace_externallink_externallinkpath = "http://purl.oclc.org/ooxml/officeDocument/relationships/externalLinkPath";
+            string namespace_externallink_oleobject = "";
             string namespace_drawing_xmlns_xdr = "http://purl.oclc.org/ooxml/drawingml/spreadsheetDrawing";
 
             using (var spreadsheet = SpreadsheetDocument.Open(input_filepath, true))
@@ -169,8 +187,6 @@ namespace CLISC
                         wbPart.SharedStringTablePart.SharedStringTable.RemoveNamespaceDeclaration("x");
                         wbPart.SharedStringTablePart.SharedStringTable.AddNamespaceDeclaration("x", namespace_xmlns);
                     }
-                    // Change namespaces in /xl/embeddings
-
                     // Change namespaces in /xl/externallinks
                     List<ExternalWorkbookPart> extwbParts = wbPart.ExternalWorkbookParts.ToList();
                     if (extwbParts.Count > 0)
@@ -183,12 +199,11 @@ namespace CLISC
                             extwbPart.RootElement.AddNamespaceDeclaration("r", namespace_xmlns_r);
                         }
                     }
-                    // Change namespaces in /docProps/app.xml
-
-
-                    // Change namespaces in /docProps/core.xml
-
-
+                    // Change namespaces in /docProps/app.xml (IS THIS WORKING?)
+                    spreadsheet.ExtendedFilePropertiesPart.Properties.RemoveNamespaceDeclaration("x");
+                    spreadsheet.ExtendedFilePropertiesPart.Properties.AddNamespaceDeclaration("x", namespace_app_xmlns);
+                    spreadsheet.ExtendedFilePropertiesPart.Properties.RemoveNamespaceDeclaration("vt");
+                    spreadsheet.ExtendedFilePropertiesPart.Properties.AddNamespaceDeclaration("vt", namespace_app_xmlns_vt);
                     // Change namespaces in /xl/theme/theme[n+1].xml
                     if (wbPart.ThemePart.Theme != null)
                     {
